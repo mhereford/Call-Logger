a682413890f511befb1ac9de0ce4e795586d481a
mhereford <mhereford@gmail.com>
2017-02-22 23:51:13 -0500

    Added list of topicFlags to Detail view.

--- src/Call-Logger/Controllers/CallLogController.cs
+++ src/Call-Logger/Controllers/CallLogController.cs
@@ -10,6 +10,19 @@ namespace Call_Logger.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.CallID = 001;
+            ViewBag.CustRep = "Greg Sherman";
+            ViewBag.CallTS = "02/22/2017";
+            ViewBag.RegistrantID = 100001;
+            ViewBag.CallStatus = "Open";
+            ViewBag.Summary = "<p>VIS password reset.  Where's my money???</p>";
+            ViewBag.TopicFlags = new string[]
+                {
+                    "Restitution: YES",
+                    "Opt-Out: NO",
+                    "Cancel Phone Call: NO"
+                };
+
             return View();
         }
     }
