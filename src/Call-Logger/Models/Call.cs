d9be49302e2aa078d6a0e45323d2981cf0c0d3f5
mhereford <mhereford@gmail.com>
2017-02-23 20:54:40 -0500

    Added Call Log Topic Flag Model.

---
+++ src/Call-Logger/Models/Call.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Call_Logger.Models
+{
+    public class Call
+    {
+        public int CallID { get; set; }
+        public string CustRep { get; set; }
+        public DateTime CallTS { get; set; }
+        public string CallStatus { get; set; }
+        public int RegistrantID { get; set; }
+        public string SummaryHTML { get; set; }
+        public TopicFlag[] TopicFlags { get; set; }
+    }
+}
---
+++ src/Call-Logger/Models/TopicFlag.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Call_Logger.Models
+{
+    public class TopicFlag
+    {
+        public string Topic { get; set; }
+        public bool TopicSelected { get; set; }
+    }
+}
