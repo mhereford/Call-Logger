a04f627bd4508e5281ac4ee2d3496456b1702bdd
mhereford <mhereford@gmail.com>
2017-02-26 00:58:34 -0500

    Added CallLogRepository.

--- src/Call-Logger/Controllers/CallLogController.cs
+++ src/Call-Logger/Controllers/CallLogController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Call_Logger.Models;
+using Call_Logger.Data;
 
 namespace Call_Logger.Controllers
 {
@@ -11,22 +12,7 @@ namespace Call_Logger.Controllers
     {
         public ActionResult Detail()
         {
-            var call = new Call()
-            {
-                CallID = 001,
-                CustRep = "Greg Sherman",
-                CallTS = "2017-02-22",
-                CallStatus = "Open",
-                SummaryHtml = "<p>VIS password reset.  Where's my money???</p>",
-                TopicFlags = new TopicFlag[]
-                {
-                    new TopicFlag() { Topic = "Restitution", TopicSelected = true },
-                    new TopicFlag() { Topic = "Opt-Out", TopicSelected = false },
-                    new TopicFlag() { Topic = "Cancel Phone Call", TopicSelected = false }
-                }
-            };
-
-          return View(call);
+            return View(call);
         }
     }
 }
---
+++ src/Call-Logger/Data/CallLogRepository.cs
@@ -0,0 +1,78 @@
+﻿using Call_Logger.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Call_Logger.Data
+{
+    public class CallLogRepository
+    {
+        private static Call[] _calls = new Call[]
+        {
+            new Call()
+            {
+                CallID = 001,
+                CustRep = "Greg Sherman",
+                CallTS = "2017-02-22",
+                CallStatus = "Open",
+                RegistrantID = 200001,
+                SummaryHtml = "<p>VIS password reset.  Where's my money???</p>",
+                TopicFlags = new TopicFlag[]
+                {
+                    new TopicFlag() { Topic = "Restitution", TopicSelected = true },
+                    new TopicFlag() { Topic = "Opt-Out", TopicSelected = false },
+                    new TopicFlag() { Topic = "Cancel Phone Call", TopicSelected = false }
+                }
+            },
+
+            new Call()
+            {
+                CallID = 002,
+                CustRep = "Greg Sherman",
+                CallTS = "2017-02-22",
+                CallStatus = "Closed",
+                RegistrantID = 200200,
+                SummaryHtml = "<p>Updated address and phone number.</p>",
+                TopicFlags = new TopicFlag[]
+                {
+                    new TopicFlag() { Topic = "Restitution", TopicSelected = false },
+                    new TopicFlag() { Topic = "Opt-Out", TopicSelected = false },
+                    new TopicFlag() { Topic = "Cancel Phone Call", TopicSelected = false }
+                }
+            },
+
+            new Call()
+            {
+                CallID = 003,
+                CustRep = "Chris Ashby",
+                CallTS = "2017-02-23",
+                CallStatus = "Closed",
+                RegistrantID = 3000000,
+                SummaryHtml = "<p>Requested to be opted out of VNS.</p>",
+                TopicFlags = new TopicFlag[]
+                {
+                    new TopicFlag() { Topic = "Restitution", TopicSelected = false },
+                    new TopicFlag() { Topic = "Opt-Out", TopicSelected = true },
+                    new TopicFlag() { Topic = "Cancel Phone Call", TopicSelected = false }
+                }
+            }
+        };
+
+        public Call GetCall(int ID)
+        {
+            Call callToReturn = null;
+
+            foreach (var call in _calls)
+            {
+                if (call.CallID == ID)
+                {
+                    callToReturn = call;
+                    break;
+                }
+
+            }
+            return callToReturn;
+        }
+    }
+}
