6c5a7f531dec66c1d37935e5d7ac044025969aeb
mhereford <mhereford@gmail.com>
2017-02-26 21:52:31 -0500

    Udated default route.

--- src/Call-Logger/App_Start/RouteConfig.cs
+++ src/Call-Logger/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Call_Logger
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "CallLog", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
